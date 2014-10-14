6ae12ea879ec5007c7cb05ece3b2e3b472243062
Michał Brewczak <michal.brewczak@gmail.com>
2014-10-14 09:39:49 +0200

    git init

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.joda.time.DateTime;
+
+/**
+ * @author Michal Brewczak
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(new DateTime().toString());
+	}
+}
